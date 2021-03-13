66594c5f02abc4e260413554dba57b96843b0963
HouariZegai <HouariZegai14@gmail.com>
2021-03-13 23:38:05 +0100

    Oauth2 resource - create the config file (application properties)

---
+++ oauth2-resource/src/main/java/com/teletic/oauth2resource/Oauth2ResourceApplication.java
@@ -0,0 +1,13 @@
+package com.teletic.oauth2resource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2ResourceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Oauth2ResourceApplication.class, args);
+    }
+
+}
