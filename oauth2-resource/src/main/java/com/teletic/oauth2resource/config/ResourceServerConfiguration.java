8c9ea4bf99983b2421789e7123b11a1a17977175
HouariZegai <HouariZegai14@gmail.com>
2021-03-13 23:41:21 +0100

    oauth2 server - made the security part

---
+++ oauth2-resource/src/main/java/com/teletic/oauth2resource/config/ResourceServerConfiguration.java
@@ -0,0 +1,41 @@
+package com.teletic.oauth2resource.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+
+import javax.servlet.http.HttpServletResponse;
+
+@Configuration
+@EnableResourceServer
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
+    @Autowired
+    @Qualifier("jwtTokenStore")
+    public TokenStore tokenStore;
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().anyRequest().permitAll()
+                .and().cors().disable()
+                .csrf().disable()
+                .httpBasic().disable()
+                .exceptionHandling()
+                .authenticationEntryPoint(
+                        (request, response, authException) -> response.sendError(HttpServletResponse.SC_UNAUTHORIZED))
+                .accessDeniedHandler(
+                        (request, response, authException) -> response.sendError(HttpServletResponse.SC_UNAUTHORIZED));
+    }
+
+    @Override
+    public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+        resources.resourceId("USER_ADMIN_RESOURCE").tokenStore(tokenStore);
+    }
+}
