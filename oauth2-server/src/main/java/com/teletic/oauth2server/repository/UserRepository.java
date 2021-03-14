8c686b48d26e5068248ce87cd083829b813a683e
HouariZegai <HouariZegai14@gmail.com>
2021-03-14 23:30:02 +0100

    oauth2 server - create the user dao

---
+++ oauth2-server/src/main/java/com/teletic/oauth2server/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.teletic.oauth2server.repository;
+
+import com.teletic.oauth2server.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
+import java.util.Optional;
+
+@Repository
+@Transactional
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUsername(String username);
+}
